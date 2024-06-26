da5af91e8c8231f988803084f9a528ac89a5ce62
beatriceconnie <beatriceconnie96@gmail.com>
2024-06-26 12:36:31 +0800

    new

---
+++ Python/functionpaarttwo.py
@@ -0,0 +1,14 @@
+def sum(a, b):
+    return a + b
+
+x = 10
+
+def authenticate(user_name, password):
+    def calculate(principle, period, rate):
+        result = principle * period * rate / 100
+        return result
+    if (user_name == "admin" and password == "leah01"):
+        print("Simple Interest: ", calculate(1000, 1, 6))
+    
+
+
---
+++ Python/test2.py
@@ -0,0 +1,7 @@
+num = int(input(""))
+
+for i in range(1, num + 1):
+    i = int(input())
+    result = result + i
+
+print(result)
